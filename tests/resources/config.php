900f989acd2a4534e4b935b6c21fda321a865454
Matej Bačo <matejbaco2000@gmail.com>
2025-11-18 15:59:20 +0100

    Devex in tests

--- tests/resources/config.php
+++ tests/resources/config.php
@@ -2,6 +2,9 @@
 
 use Utopia\Config\Config;
 
+// Comment line and few empty lines, ensure parser doesnt break
+// 
+
 return [
     'key' => 'keyValue',
     'keyWithComment' => 'keyWithCommentValue', // A comment
